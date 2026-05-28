d9aa3a46792e95dd017b6af32d3a8c7c4e20f967
9oleo9 <leomartelli44@gmail.com>
2026-05-28 12:30:39 +0200

    256

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pyxel as p
 
 class App:
     def __init__(self):
-        p.init(128, 128, title="Tower Defense")
+        p.init(256, 256, title="Tower Defense")
         p.run(self.update, self.draw)
 
     def update(self):
