395d0321a349d077fb1d2a9b8446a425c25d69fe
SaurFort <tou.nathan@gmail.com>
2026-05-28 12:26:07 +0200

    Base

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-import pyxel
+import pyxel as p
+
+class App:
+    def __init__(self):
+        p.init(128, 128, title="Tower Defense")
+        p.run(self.update, self.draw)
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
