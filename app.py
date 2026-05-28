37a4768d4edfa395b4446bc2d1bfa952d47e928c
SaurFort <tou.nathan@gmail.com>
2026-05-28 12:46:30 +0200

    Ajout objet app

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ import pyxel as p
 class App:
     def __init__(self):
         p.init(256, 256, title="Tower Defense")
+
+        self.map = Map()
+        self.joueur = Joueur()
+
         p.run(self.update, self.draw)
 
     def update(self):
         pass
 
     def draw(self):
-        pass
+        self.map.draw()
 
 class Map:
     def __init__(self):
